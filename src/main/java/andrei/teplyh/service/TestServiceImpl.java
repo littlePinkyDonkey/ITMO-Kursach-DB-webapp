22d1bad177ccd1461398d7fde778473c71d623dd
littlePinkyDonkey <teplyh.andre@yandex.ru>
2021-01-20 19:32:34 +0300

    cleaned TestService

--- src/main/java/andrei/teplyh/service/TestServiceImpl.java
+++ src/main/java/andrei/teplyh/service/TestServiceImpl.java
@@ -68,54 +68,6 @@ public class TestServiceImpl implements TestService {
     @Override
     @Transactional
     public void showIfWorks() {
-        //workersRepository.findAll().forEach(System.out::println);
-//        System.out.println("************************");
-//        storyboardArtistRepository.findAll().forEach(System.out::println);
-
-//        artistsRepository.findAll().forEach(System.out::println);
-        //storyboardArtistRepository.findAll().forEach(System.out::println);
-        //rolesDesignerRepository.findAll().forEach(System.out::println);
-        //producerRepository.findAll().forEach(System.out::println);
-//        userRepository.findAll().forEach(System.out::println);
-        //processRepository.findAll().forEach(System.out::println);
-//        boolean test =
-//                producerRepository.addExistingProducer(12, ProducerRoles.ASSOCIATE_PRODUCER.getDescription());
-//        System.out.println(test);
-//        advertisingProcessRepository.findAll().forEach(System.out::println);
-
-//        workersRepository.addWorker("andrey", "teplyh", "male", 20, "russia");
-//
-        Date date = new Date();
-
-        userRepository.deleteAll();
-        workersRepository.deleteAll();
-        storyboardProcessRepository.deleteAll();
-        artifactRepository.deleteAll();
-
-        //userRepository.addUser(1, "andrei", "admin","dasdsa","dsada", date);
-
-//        plotProcessRepository.createPlotProcess(12, new Timestamp(date.getTime()), "dadas",
-//                ProcessStatuses.FINISHED.getDescription(), Timestamp.valueOf("2012-04-10 17:57:08.8"));
-        int workerId = workersRepository.addWorker("andrei", "teplyh", "male", 20, "Russia");
-        int storyboarderId = storyboardArtistRepository.addExistingStoryboardArtist(workerId);
-
-        storyboardProcessRepository.createStoryboardProcess(20, new Timestamp(date.getTime()), "blablabla",
-                ProcessStatuses.FINISHED.getDescription(), Timestamp.valueOf("2012-04-10 17:57:08.8"), 20);
-
-        int artifactId = artifactRepository.createArtifact(workerId, ArtifactTypes.TEXT.getDescription(), 20, new Timestamp(date.getTime()), "~/");
-
-
-
-        int userId = userRepository.addUser(workerId, "andrei", "admin","dasdsa","dsada", date);
-
-        User user = userRepository.getUserByLogin("andrei");
-
-        List<PlotProcess> plotProcesses = plotProcessRepository.findAll();
-        List<Plot> plots = plotRepository.findAll();
-        System.out.println("\ndsadsa");
-
-//        testRepository.test3("stan", AbilityTypes.CHATTING.getDescription());
-//        List<Test> tests =  testRepository.test2();
-//        tests.forEach(System.out::println);
+        
     }
 }
