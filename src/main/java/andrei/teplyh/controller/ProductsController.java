8a66aaf92ec5a921439cf65599ef62b60182896e
GulDilin <zhenyagurin@gmail.com>
2021-01-23 07:51:00 +0300

    Return Get instead of Post

--- src/main/java/andrei/teplyh/controller/ProductsController.java
+++ src/main/java/andrei/teplyh/controller/ProductsController.java
@@ -19,7 +19,7 @@ public class ProductsController {
         this.productService = productService;
     }
 
-    @PostMapping(produces = "application/json")
+    @GetMapping(produces = "application/json")
     public ResponseEntity getAllProducts(@RequestAttribute(name = "login") String login) {
         try {
             List<ProductDTO> response = productService.loadProductsByUsername(login);
