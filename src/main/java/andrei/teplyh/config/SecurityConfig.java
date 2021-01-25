45fd6bd6bd093254e4f69fd1f6268c781a84b8c1
littlePinkyDonkey <teplyh.andre@yandex.ru>
2021-01-25 20:46:43 +0300

    configured access to process controllers by roles

--- src/main/java/andrei/teplyh/config/SecurityConfig.java
+++ src/main/java/andrei/teplyh/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package andrei.teplyh.config;
 
+import andrei.teplyh.entity.User;
+import andrei.teplyh.entity.enums.UserRoles;
 import andrei.teplyh.security.jwt.config.JwtConfigurer;
 import andrei.teplyh.security.jwt.util.JwtTokenProvider;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -36,17 +38,37 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter implements WebM
                 .httpBasic().disable()
                 .csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                
                 .and()
                 .authorizeRequests()
                 .antMatchers("/api/roles").permitAll()
                 .antMatchers("/api/login").permitAll()
                 .antMatchers("/api/registration").permitAll()
+                .antMatchers("/api/processes/ability_description_process/**").hasRole(UserRoles.ROLES_DESIGNER.toString())
+                .antMatchers("/api/processes/character_description_process/**").hasRole(UserRoles.ROLES_DESIGNER.toString())
+                .antMatchers("/api/processes/adding_effect_process/**").hasRole(UserRoles.ARTIST.toString())
+                .antMatchers("/api/processes/animation_process/**").hasRole(UserRoles.ARTIST.toString())
+                .antMatchers("/api/processes/battle_drawing_process/**").hasRole(UserRoles.ARTIST.toString())
+                .antMatchers("/api/processes/character_drawing_process/**").hasRole(UserRoles.ARTIST.toString())
+                .antMatchers("/api/processes/coloring_process/**").hasRole(UserRoles.ARTIST.toString())
+                .antMatchers("/api/processes/adding_sound_process/**").hasRole(UserRoles.AUDIO_SPECIALIST.toString())
+                .antMatchers("/api/processes/advertising_process/**").hasRole(UserRoles.PRODUCER.toString())
+                .antMatchers("/api/processes/battle_description_process/**").hasRole(UserRoles.SCREENWRITER.toString())
+                .antMatchers("/api/processes/plot_process/**").hasAnyRole(UserRoles.SCREENWRITER.toString(), UserRoles.REGISSEUR.toString())
+                .antMatchers("/api/processes/location_description_process/**").hasRole(UserRoles.REGISSEUR.toString())
+                .antMatchers("/api/processes/character_select_process/**").hasRole(UserRoles.EDITOR.toString())
+                .antMatchers("/api/processes/digitization_process/**").hasRole(UserRoles.DIGITIZER.toString())
+                .antMatchers("/api/processes/revisions_process/**").hasRole(UserRoles.ART_DIRECTOR.toString())
+                .antMatchers("/api/processes/smoothing_process/**").hasRole(UserRoles.SMOOTHING_SPECIALIST.toString())
+                .antMatchers("/api/processes/storyboard_process/**").hasRole(UserRoles.STORYBOARD_ARTIST.toString())
+                .antMatchers("/api/processes/voice_acting_process/**").hasRole(UserRoles.RECORDING_ACTOR.toString())
                 .antMatchers("/*.png").permitAll()
                 .antMatchers("/*.jpg").permitAll()
                 .anyRequest().authenticated()
+
                 .and()
                 .apply(new JwtConfigurer(jwtTokenProvider));
-    }
+        }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
